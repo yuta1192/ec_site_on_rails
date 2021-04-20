4ccfaa0de020f6f24e42684195f928bca964c65e
yuta <yuta.s1182@gmail.com>
2021-04-20 15:27:37 +0900

    komentoauto

--- db/migrate/20200614212724_add_column_to_information.rb
+++ db/migrate/20200614212724_add_column_to_information.rb
@@ -1,13 +1,13 @@
 class AddColumnToInformation < ActiveRecord::Migration[5.2]
   def change
-    add_column :informations, :release_flg, :boolean, default: true, null: false
-    add_column :informations, :published_start, :date
-    add_column :informations, :published_end, :date
-    add_column :informations, :attachment_file1, :string
-    add_column :informations, :attachment_file2, :string
-    add_column :informations, :attachment_file3, :string
-    add_column :informations, :attachment_file4, :string
-    add_column :informations, :attachment_file5, :string
-    remove_column :informations, :image
+    # add_column :Information, :release_flg, :boolean, default: true, null: false
+    # add_column :Information, :published_start, :date
+    # add_column :Information, :published_end, :date
+    # add_column :Information, :attachment_file1, :string
+    # add_column :Information, :attachment_file2, :string
+    # add_column :Information, :attachment_file3, :string
+    # add_column :Information, :attachment_file4, :string
+    # add_column :Information, :attachment_file5, :string
+    # remove_column :Information, :image
   end
 end
