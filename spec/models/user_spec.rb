251351dd3c9b725784e2865ed10c0abceb3aefc1
yuta <yuta.s1182@gmail.com>
2021-05-04 20:20:02 +0900

    config.yml修正

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "name と email を持ったユーザーを登録できること" do
+    user = User.new(
+      name_sei: "John",
+      e_mail: "john@example.com",
+      )
+     # オブジェクトをexpectに渡す動作が有効であることを確認
+     expect(user).not_to be_valid
+  end
+end
