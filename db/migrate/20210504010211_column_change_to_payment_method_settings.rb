262e845d8b190fd6ca4aeb2056da7cb5fe760f44
yuta <yuta.s1182@gmail.com>
2021-05-28 18:32:49 +0900

    herokuerrorのため

--- db/migrate/20210504010211_column_change_to_payment_method_settings.rb
+++ db/migrate/20210504010211_column_change_to_payment_method_settings.rb
@@ -1,17 +1,17 @@
 class ColumnChangeToPaymentMethodSettings < ActiveRecord::Migration[5.2]
   def change
-    change_column :payment_method_settings, :amount_collected_1, :integer
-    change_column :payment_method_settings, :cash_on_delivery_charge_1, :integer
-    change_column :payment_method_settings, :amount_collected_2, :integer
-    change_column :payment_method_settings, :cash_on_delivery_charge_2, :integer
-    change_column :payment_method_settings, :amount_collected_3, :integer
-    change_column :payment_method_settings, :cash_on_delivery_charge_3, :integer
-    change_column :payment_method_settings, :amount_collected_4, :integer
-    change_column :payment_method_settings, :cash_on_delivery_charge_4, :integer
-    change_column :payment_method_settings, :amount_collected_5, :integer
-    change_column :payment_method_settings, :cash_on_delivery_charge_5, :integer
-    change_column :payment_method_settings, :amount_collected_6, :integer
-    change_column :payment_method_settings, :cash_on_delivery_charge_6, :integer
-    change_column :payment_method_settings, :cash_on_delivery_charge_7, :integer
+    # change_column :payment_method_settings, :amount_collected_1, :integer
+    # change_column :payment_method_settings, :cash_on_delivery_charge_1, :integer
+    # change_column :payment_method_settings, :amount_collected_2, :integer
+    # change_column :payment_method_settings, :cash_on_delivery_charge_2, :integer
+    # change_column :payment_method_settings, :amount_collected_3, :integer
+    # change_column :payment_method_settings, :cash_on_delivery_charge_3, :integer
+    # change_column :payment_method_settings, :amount_collected_4, :integer
+    # change_column :payment_method_settings, :cash_on_delivery_charge_4, :integer
+    # change_column :payment_method_settings, :amount_collected_5, :integer
+    # change_column :payment_method_settings, :cash_on_delivery_charge_5, :integer
+    # change_column :payment_method_settings, :amount_collected_6, :integer
+    # change_column :payment_method_settings, :cash_on_delivery_charge_6, :integer
+    # change_column :payment_method_settings, :cash_on_delivery_charge_7, :integer
   end
 end
