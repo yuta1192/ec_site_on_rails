a13b7325bbc27d75793ed16c8675267e8f1971c5
yuta <yuta.s1182@gmail.com>
2021-05-31 00:30:38 +0900

    changeからremoveしてaddする様に変更

--- db/migrate/20210504010211_column_change_to_payment_method_settings.rb
+++ db/migrate/20210504010211_column_change_to_payment_method_settings.rb
@@ -1,17 +1,32 @@
 class ColumnChangeToPaymentMethodSettings < ActiveRecord::Migration[5.2]
   def change
-    change_column :payment_method_settings, :amount_collected_1, :integer
-    change_column :payment_method_settings, :cash_on_delivery_charge_1, :integer
-    change_column :payment_method_settings, :amount_collected_2, :integer
-    change_column :payment_method_settings, :cash_on_delivery_charge_2, :integer
-    change_column :payment_method_settings, :amount_collected_3, :integer
-    change_column :payment_method_settings, :cash_on_delivery_charge_3, :integer
-    change_column :payment_method_settings, :amount_collected_4, :integer
-    change_column :payment_method_settings, :cash_on_delivery_charge_4, :integer
-    change_column :payment_method_settings, :amount_collected_5, :integer
-    change_column :payment_method_settings, :cash_on_delivery_charge_5, :integer
-    change_column :payment_method_settings, :amount_collected_6, :integer
-    change_column :payment_method_settings, :cash_on_delivery_charge_6, :integer
-    change_column :payment_method_settings, :cash_on_delivery_charge_7, :integer
+    # remove
+    remove_column :payment_method_settings, :amount_collected_1
+    remove_column :payment_method_settings, :cash_on_delivery_charge_1
+    remove_column :payment_method_settings, :amount_collected_2
+    remove_column :payment_method_settings, :cash_on_delivery_charge_2
+    remove_column :payment_method_settings, :amount_collected_3
+    remove_column :payment_method_settings, :cash_on_delivery_charge_3
+    remove_column :payment_method_settings, :amount_collected_4
+    remove_column :payment_method_settings, :cash_on_delivery_charge_4
+    remove_column :payment_method_settings, :amount_collected_5
+    remove_column :payment_method_settings, :cash_on_delivery_charge_5
+    remove_column :payment_method_settings, :amount_collected_6
+    remove_column :payment_method_settings, :cash_on_delivery_charge_6
+    remove_column :payment_method_settings, :cash_on_delivery_charge_7
+    # add
+    add_column :payment_method_settings, :amount_collected_1, :integer
+    add_column :payment_method_settings, :cash_on_delivery_charge_1, :integer
+    add_column :payment_method_settings, :amount_collected_2, :integer
+    add_column :payment_method_settings, :cash_on_delivery_charge_2, :integer
+    add_column :payment_method_settings, :amount_collected_3, :integer
+    add_column :payment_method_settings, :cash_on_delivery_charge_3, :integer
+    add_column :payment_method_settings, :amount_collected_4, :integer
+    add_column :payment_method_settings, :cash_on_delivery_charge_4, :integer
+    add_column :payment_method_settings, :amount_collected_5, :integer
+    add_column :payment_method_settings, :cash_on_delivery_charge_5, :integer
+    add_column :payment_method_settings, :amount_collected_6, :integer
+    add_column :payment_method_settings, :cash_on_delivery_charge_6, :integer
+    add_column :payment_method_settings, :cash_on_delivery_charge_7, :integer
   end
 end
