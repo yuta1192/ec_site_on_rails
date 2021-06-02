ca5767ad27d2b4e911f89841a1d4fb5bb80e5486
yuta <yuta.s1182@gmail.com>
2021-06-02 19:54:59 +0900

    a

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 require("jquery");
 require("bootstrap");
+import "./app/assets/stylesheets/application.scss";
+import '../../assets/stylesheets/application.scss';
 import 'bootstrap';
 import '../src/style.scss';
 import '../stylesheets/application';
-import "styles/application.scss";
