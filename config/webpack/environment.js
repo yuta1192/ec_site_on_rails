00604c2b60fc4f7d87a68427bac0682980f68eab
yuta <yuta.s1182@gmail.com>
2021-06-03 11:55:23 +0900

    a

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,14 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require(‘@rails/webpacker’)
+const path = require(‘path’)
+
+// sass-loaderの設定を取得
+const sassLoaders = environment.loaders.get(‘sass’)
+const sassLoaderConfig = sassLoaders.use.filter(config => config.loader === ‘sass-loader’)[0]
+
+// sass-loaderに自動ロードの設定をがっちゃんこする
+Object.assign(sassLoaderConfig.options, {
+data: “@import ‘global-imports.scss’;”,
+includePaths: [path.resolve(__dirname, ‘../../app/javascript/stylesheets/’)]
+})
 
 module.exports = environment
-
-const webpack = require('webpack')
-environment.plugins.append(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-  })
-)
