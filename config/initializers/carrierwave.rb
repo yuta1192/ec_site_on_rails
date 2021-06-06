e857588d6dc19648c900836fa1f556018b11e21b
yuta <yuta.s1182@gmail.com>
2021-06-06 12:23:51 +0900

    s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.production? # 本番環境:AWS使用
+  # if Rails.env.production? # 本番環境:AWS使用
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'superecsite1'
@@ -18,8 +18,8 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1',
       path_style: true
     }
-  else
-    config.storage :file # 開発環境:public/uploades下に保存
-    config.enable_processing = false if Rails.env.test? #test:処理をスキップ
-  end
+  # else
+  #   config.storage :file # 開発環境:public/uploades下に保存
+  #   config.enable_processing = false if Rails.env.test? #test:処理をスキップ
+  # end
 end
